16fefe1425a55d1ce3259795a520dced6f4bfe6a
emCog <robin.leonard@gmail.com>
2020-04-18 09:42:23 +0100

    first deploy
    preview WIP for David

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  theme: {
+        height: {
+            vh25: '25vh',
+            vh50: '50vh',
+            vh75: '75vh',
+            vh100: '100vh',
+
+         // gutallBanner: '700px',
+    }
+  }
+}
+
+// Res
+//     extend: {},
+//   },
+//   variants: {},
+//   plugins: [],
+// }
